4f41d225ce9749cfa3ed255248d7c20fa6e61899
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-04-04 02:10:10 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(t.length()!=s.length()){
+            return false;
+        }
+        HashSet<Character> set =  new HashSet<>();
+        for(int i =0;i<s.length();i++){
+            set.add(s.charAt(i));
+        }
+        
+        for(int i =0;i<t.length();i++){
+            if(set.contains(t.charAt(i))){
+                set.remove(t.charAt(i));
+            };
+        }
+        return set.size()==0 ? true : false;
+    }
+}
