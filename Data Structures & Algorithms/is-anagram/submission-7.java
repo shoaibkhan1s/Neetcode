35237f4cbfcbd9157bb4e231c492c5ce64ba51d0
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-04-04 02:13:21 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(t.length()!=s.length()){
+            return false;
+        }
+        HashSet<Character> set =  new HashSet<>();
+        for(int i =0;i<s.length();i++){
+            set.add(s.charAt(i));
+        }
+        
+        for(int i =0;i<t.length();i++){
+            if(!set.contains(t.charAt(i))){ 
+                return false;
+            }
+            
+            if(set.contains(t.charAt(i))){
+                set.remove(t.charAt(i));
+            };
+        }
+        return set.size()==0 ? true : false;
+    }
+}
