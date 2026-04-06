966b6522c24d8b7a40e0c16017fadf592f983ccc
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-04-06 23:55:17 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+    }
+    public boolean isValidRow(char[][] board,int r){
+
+        for(int i=0;i<9;i++){
+            
+        }
+    }
+}
