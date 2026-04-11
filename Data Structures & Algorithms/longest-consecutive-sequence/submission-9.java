5876fd93424b72bca7f77dff002ea84b2a5c5c92
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-04-11 19:16:23 +0530

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.java
@@ -0,0 +1,24 @@
+class Solution {
+           	 public static int longestConsecutive(int[] arr) {
+                      Set<Integer> set = new HashSet<>();
+                           for(int i =0;i<arr.length;i++){
+                                   set.add(arr[i]);
+                                        }
+                                             int maxLen=0;
+                                                     for(int num:arr){
+                                                                 if(!set.contains(num-1)){
+                                                                                 int curr = num;
+                                                                                                 int len = 1;
+
+                                                                                                                 while(set.contains(curr+1)){
+                                                                                                                                     curr++;
+                                                                                                                                                         len++;
+                                                                                                                                                                         }
+                                                                                                                                                                                         maxLen = Math.max(maxLen,len);
+                                                                                                                                                                                                     }
+                                                                                                                                                                                                             }
+                                                                                                                                                                                                                     return maxLen;
+                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                 }
+                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                     }
