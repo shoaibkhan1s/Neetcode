bff80af605fb7942fe9f5a43bcf6a4e9e951b784
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-04-14 13:26:21 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,29 @@
+class Solution{
+public int trap(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+    int leftMax = 0;
+    int rightMax = 0;
+    int water = 0;
+
+    while (left <= right) {
+        if (arr[left] <= arr[right]) {
+            if (arr[left] >= leftMax) {
+                leftMax = arr[left];
+            } else {
+                water += leftMax - arr[left];
+            }
+            left++;
+        } else {
+            if (arr[right] >= rightMax) {
+                rightMax = arr[right];
+            } else {
+                water += rightMax - arr[right];
+            }
+            right--;
+        }
+    }
+
+    return water;
+}
+}
