f97f5828b83417a67c269c44765f8796ce0180fa
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-05-05 19:49:28 +0530

    Fix indentation and formatting in submission-1.java

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -10,8 +10,8 @@ class Solution {
                                                                                                     }
                                                                                                             }
                                                                                                                     return profit;
-                                                                                                                        }
-                                                                                                                        }
+                                                      }
+                                            }
                                                                                                                         
 }
 
