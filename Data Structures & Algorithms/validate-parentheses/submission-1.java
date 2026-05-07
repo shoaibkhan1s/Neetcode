6aa5c342e7bafbe2081eaa5eaecc4d01383b841a
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-05-08 01:08:51 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char s : str.toCharArray()){
+            if(s=='(' || s=='{' || s=='['){
+                stack.push(s);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if( (s==')' && top!='(') || (s=='}' && top!='{') || (s==']' && top!='[') ){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
