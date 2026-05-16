0980b01ef2279029b96cc15830405e43e45d74ad
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-05-16 18:13:13 +0530

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int peek = findPeek(arr);
+        if(peek==-1){
+            return search(arr,0,arr.length-1,target);
+        }
+        if(target>=arr[0] && target<=arr[peek]){
+            return search(arr,0,peek,target);
+        }else{
+            return search(arr,peek+1,arr.length-1,target);
+        }
+
+    }
+
+    public int search(int[] arr, int s, int e, int target){
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]>target){
+                e = mid-1;
+            } else if(arr[mid]<target){
+                s = mid+1;
+            } else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public int findPeek(int[] arr){
+        int s = 0;
+        int e = arr.length-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(mid<e && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(mid>s && arr[mid]<arr[mid-1]){
+                return mid-1;
+            } else if(arr[s]>arr[mid]){
+                e = mid-1;
+            }else{
+                s = mid+1;
+            }
+        }
+        return -1;
+    }
+}
