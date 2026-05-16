3ff37ec0c0034f8dc969438e2f2f9cbe1fdfc1ab
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-05-16 23:53:04 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.java
@@ -0,0 +1,50 @@
+class Pair{
+    String value;
+    int timeStamp;
+    Pair(String value,int timeStamp){
+        this.value = value;
+        this.timeStamp = timeStamp;
+    }
+    int getTime(){
+        return timeStamp;
+    }
+    String getValue(){
+        return value;
+    }
+}
+
+class TimeMap {
+  HashMap<String, List<Pair>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+       
+       //create a new Arraylist at the given key only when arraylist is not created
+        map.computeIfAbsent(key,k->new ArrayList<>());
+
+        map.get(key).add(new Pair(value,timestamp));
+
+    }
+
+    public String get(String key, int timestamp) {
+       List<Pair> pairs =  map.get(key);
+
+        int s =0;
+        int e = pairs.size()-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(pairs.get(mid).getTime()==timestamp){
+                return pairs.get(mid).getValue();
+            }
+            else if(pairs.get(mid).getTime()>timestamp){
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
+        }
+        return e!=-1? pairs.get(e).getValue() :pairs.get(0).getValue() ;
+    }
+}
