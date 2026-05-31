f2e1de64ce2858ada5c8d497586b15f775ccc805
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-05-31 12:25:43 +0530

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,15 @@
+class Solution {
+        public int maxProfit(int[] prices) {
+                int minPrice = Integer.MAX_VALUE;
+                        int profit =0;
+                                for(int i=0;i<prices.length;i++){
+                                            if(prices[i]<minPrice){
+                                                            minPrice = prices[i];
+                                                                        }else if(profit<prices[i]-minPrice){
+                                                                                        profit = prices[i]-minPrice;
+                                                                                                    }
+                                                                                                            }
+                                                                                                                    return profit;
+                                                                                                                        }
+                                                                                                                        }
+
