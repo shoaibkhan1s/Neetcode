5fd43f1bba6efbd142b4efef4828e35cf0221329
MOHAMMAD SHOAIB <170645833+shoaibkhan1s@users.noreply.github.com>
2026-09-22 00:43:28 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] cost) {
+        int n = cost.length;
+        if(n==1){
+            return cost[0];
+        }
+     int[] p = new int[n];
+     p[0] = cost[0];
+     p[1] = Math.max(cost[0],cost[1]);
+     for(int i=2;i<cost.length;i++){
+        p[i] = Math.max(cost[i], cost[i]+p[i-2]);
+     }
+     return p[n-1];
+    }
+}
